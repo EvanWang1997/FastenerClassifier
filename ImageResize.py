1b488f8396c19b801d7041c319656417f4db995c
Evan Wang <evan7@students.cs.ubc.ca>
2022-03-24 16:16:38 -0700

    Added API for converting entire folder to a specified percentage:
    ImageResize.resize_all()
    
    Will also resize images nested in sub folders

--- ImageResize.py
+++ ImageResize.py
@@ -1,19 +1,25 @@
 import cv2
+import os
+
 
 class ImageResizer:
 
     def __init__(self):
         self.datapath = "./Data/"
 
-    def resize_percent(self, original_file_name, conv_file_name, percent):
+    """
+    Resizes a single image file, creates a new image file scaled down to percent
+    :param data_folder: String name of data folder 
+    :param original_file_name: String name of original file
+    :param conv_file_name: String name of scaled down image file
+    :param percent: Num percent to scale down to for original image file
+    """
+    def resize_percent(self, orig_data_folder, orig_file_name, conv_data_folder, conv_file_name, percent):
         print("Running image conversion")
 
         # Gets original image from specified file in data
-        full_file_path = self.datapath + original_file_name
+        full_file_path = orig_data_folder + orig_file_name
         img = cv2.imread(full_file_path, cv2.IMREAD_UNCHANGED)
-
-        # Prints original file dimensions
-        print('Original Dimensions : ', img.shape)
 
         # Calculates converted dimensions
         width = int(img.shape[1] * percent / 100)
@@ -22,13 +28,43 @@ class ImageResizer:
 
         # resize image
         resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-        print('Resized Dimensions : ', resized.shape)
 
-        full_new_file_path = self.datapath + conv_file_name
+        # Creates converted data folder if converted data folder did not exist
+        if not os.path.exists(conv_data_folder):
+            os.makedirs(conv_data_folder)
 
-        cv2.imshow("Resized image", resized)
-        cv2.waitKey(0)
+        # Saves resized image to converted data folder
+        full_new_file_path = conv_data_folder + conv_file_name
         cv2.imwrite(full_new_file_path, resized)
-        cv2.destroyAllWindows()
+
+    """
+    Converts all image data inside of a data folder into a new size, puts it into new folder
+    :param data_folder: String name of data folder 
+    :param new_folder: String name of new data folder to put the data into
+    :param percent: Num percent to scale down to for each image file
+    """
+    def resize_all(self, data_folder, new_folder, percent):
+
+        # Creates a new folder if the new folder name did not exist
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
+
+        # Lops through all files and folders in the root folder
+        for root, dirs, files in os.walk(data_folder):
+            # Resizes all files in that specific folder
+            for filename in files:
+                self.resize_percent(data_folder, filename, new_folder, filename, percent)
+
+            # Loops through all folders in this main folder
+            for folder in dirs:
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+
+                sub_folder = data_folder + folder
+                new_sub_folder = new_folder + folder
+                self.resize_all(sub_folder, new_sub_folder, percent)
+
+
+
 
 
--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn as sklearn
 import cv2
+
 from ImageResize import ImageResizer
 
 
 if __name__ == '__main__':
 
     IR = ImageResizer()
+    IR.resize_all("./Data/DataTest/", "./Data/Data5%/", 5)
 
-    IR.resize_percent("Test2.jpg", "Test2_10%.jpg", 10)
+
