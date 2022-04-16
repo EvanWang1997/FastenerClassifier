d3cbb9eece9ec8c1dd89977d3e2bae95a8ee6649
Jeremy <denglongfa@gmail.com>
2022-04-15 18:39:50 -0700

    Push dammit

--- BoltClassifier.py
+++ BoltClassifier.py
@@ -9,12 +9,17 @@ class BoltClassifier:
         self.model = models.Sequential()
 
     def create_model(self):
-        self.model.add(layers.Conv2D(288, (3, 3), activation='relu', input_shape=(288, 216, 1)))
-        self.model.add(layers.MaxPooling2D((2, 2)))
-        self.model.add(layers.Conv2D(288, (3, 3), activation='relu'))
-        self.model.add(layers.MaxPooling2D((2, 2)))
-        self.model.add(layers.Conv2D(288, (3, 3), activation='relu'))
+        self.model.add(layers.Conv2D(32, 3, padding="same", activation="relu", input_shape=(288, 216, 1)))
+        self.model.add(layers.MaxPool2D())
+
+        self.model.add(layers.Conv2D(32, 3, padding="same", activation="relu"))
+        self.model.add(layers.MaxPool2D())
+
+        self.model.add(layers.Conv2D(64, 3, padding="same", activation="relu"))
+        self.model.add(layers.MaxPool2D())
+        self.model.add(layers.Dropout(0.4))
+
         self.model.add(layers.Flatten())
-        self.model.add(layers.Dense(288, activation='relu'))
-        self.model.add(layers.Dense(11))
+        self.model.add(layers.Dense(128, activation="relu"))
+        self.model.add(layers.Dense(11, activation="softmax"))
         return self.model
