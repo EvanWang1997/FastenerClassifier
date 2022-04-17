17e54c9b7c9713253403d5c88fec0a18542a1970
Evan Wang <evan7@students.cs.ubc.ca>
2022-04-17 15:10:51 -0700

    Added the class RandomForestModels.py, contains the following:
    - createModels: Creates a list of models based on the inputted model creation function, and stores them both persistently and in a local object array
    
    - loadModels: Loads all of the models at a specified disk location into the instance of the RandomForestModels.py
    
    - predictValues: Performs voting prediction using all of the models stored, returns highest probability class between all models

---
+++ RandomForestModels.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+import keras as ks
+import os
+import numpy as np
+
+class RandomForestModels:
+
+    def __init__(self):
+        self.models = []
+        self.nummodels = 0
+        return
+
+    # Function for creating models from keras
+    # Params:
+    # modelfolder: Name of folder to persistently store all of the models desired
+    # nummodels: Number of models desired for the array of models
+    # kmfunction: Keras model creation function
+    # xtrain: training input data for each of the models (images)
+    # ytrain: training output data for each of the models (bolt class)
+    # epochs: Number of epochs to be trained for, for each model
+    # testeingdata: testing data used to validate each model during the epochs
+    def createModels(self, modelsfolder, nummodels, kmfunction, xtrain, ytrain, epochs, testingdata):
+
+        self.nummodels = nummodels
+
+        if not os.path.exists(modelsfolder):
+            os.makedirs(modelsfolder)
+
+        for i in range(nummodels):
+            model = kmfunction()
+            model.fit(xtrain, ytrain, epochs, testingdata)
+            self.models[i] = model
+            model.save(modelsfolder + "/" + i)
+
+
+    # Loads models from the specified models folder
+    # Stores them all into the models array for an instance of this class
+    # Params:
+    # modelsfolder: string specifying the folder containing the models to be loaded in
+    def loadModels(self, modelsfolder):
+        self.models = []
+        self.nummodels = 0
+
+        for folder in os.listdir(modelsfolder):
+            print(self.nummodels)
+            self.models.append(ks.models.load_model(modelsfolder + "/" + folder))
+            self.nummodels += 1
+
+
+    # Gathers predictions based on each of the models stored in the class
+    # Params:
+    # X: Data whose classes we want to predict on
+    def predictValues(self, X):
+        n = np.shape(X)[0]
+
+        print(X.shape)
+
+        if (self.nummodels > 0):
+            y1 = self.models[0].predict(X)
+            k, c = np.shape(y1)
+        else:
+            return
+
+        predprob = np.zeros((n, c))
+
+        for i in range(self.nummodels):
+            predprob += self.models[i].predict(X)
+
+        y_pred = np.argmax(predprob, axis=1)
+        return y_pred
--- main.py
+++ main.py
@@ -9,19 +9,17 @@ import cv2
 import tensorflow as tf
 import keras.utils as ku
 import keras
+
 from sklearn.metrics import classification_report
 
 from ImageResize import ImageResizer
 from BoltClassifier import BoltClassifier
+from RandomForestModels import RandomForestModels
 
 if __name__ == '__main__':
     np.random.seed(69420)
     IR = ImageResizer()
-    # IR.greyscale_all("./Data/Classes25%/", "./Data/Classesgrey25%/")
-    # IR.resize_all("./Data/Classes/", "./Data/Classes25%/", 25)
-    # IR.convert_folder_classes("./Data/Classes25%/", "25%.pkl")
     data = IR.load_data("./Data/grey10%parallel.pkl")
-    print(data[0])
     v, h = np.shape(data)
     np.random.shuffle(data)
     train, test, validate = np.split(data, [int(.6 * v), int(.9 * v)])
@@ -34,28 +32,29 @@ if __name__ == '__main__':
     X_validate = (validate[:, :h - 1]) / 255
     X_validate = X_validate.reshape(np.shape(X_validate)[0], 288, 216, 1)
     y_validate = validate[:, h - 1]
-    # X = data[:, :h - 1].astype(float)
-    # X = X.reshape(v, 288, 216, 1)
-    # y = np.core.defchararray.replace(data[:, h - 1], '-', '')
-    # y = y.astype(int)
-    print(type(X_train[1][1][1][0]))
-    print(type(y_train[1]))
     BC = BoltClassifier()
 
-    # model = keras.models.load_model("./Models/prelim_model_parallel")
+    FM = RandomForestModels()
+    FM.loadModels("./Models")
 
-    model = BC.create_model()
-    model.summary()
+    # model1 = keras.models.load_model("./Models/prelim_model_1")
+    # model2 = keras.models.load_model("./Models/prelim_model_2")
+    # model3 = keras.models.load_model("./Models/prelim_model_3")
+    # model4 = keras.models.load_model("./Models/prelim_model_4")
+    # model5 = keras.models.load_model("./Models/prelim_model_5")
 
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+    y_pred = FM.predictValues(X_validate)
 
-    history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
-    model.save("./Models/prelim_model_parallel")
+    # model = BC.create_model()
+    # model.summary()
+    #
+    # model.compile(optimizer='adam',
+    #               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    #               metrics=['accuracy'])
+    #
+    # history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+    # model.save("./Models/prelim_model_5")
 
-    y_pred = model.predict(X_test)
-    y_pred = np.argmax(y_pred, axis=1)
     print(y_test)
     # model.evaluate(X_test, y_test)
-    print(classification_report(y_test, y_pred))
+    print(classification_report(y_validate, y_pred))
