4c5c16d3b1186034762b97066d48f776219a1683
Jeremy <denglongfa@gmail.com>
2022-04-18 14:57:55 -0700

    data augmentation on training set only

---
+++ DataAugmentation.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from copy import deepcopy
+
+
+class DataAugmentation:
+
+    def __init__(self):
+        self.da = ImageDataGenerator(rotation_range=90, horizontal_flip=True, vertical_flip=True)
+
+    def aug_data(self, X, y, batch_size):
+        X_copy = deepcopy(X)
+        data_aug = self.da.flow(X_copy, y, batch_size=batch_size)
+        X_batch, y_batch = data_aug.next()
+        X_concat = np.concatenate((X, X_batch), axis=0)
+        y_concat = np.concatenate((y, y_batch))
+        return X_concat, y_concat
--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import os
 from pathlib import Path
 
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 import sklearn as sklearn
 import cv2
 import tensorflow as tf
-import keras.utils as ku
-import keras
+from keras import layers, models
 from sklearn.metrics import classification_report
 
 from ImageResize import ImageResizer
 from BoltClassifier import BoltClassifier
+from DataAugmentation import DataAugmentation
 
 if __name__ == '__main__':
     np.random.seed(69420)
@@ -20,39 +20,35 @@ if __name__ == '__main__':
     # IR.greyscale_all("./Data/Classes25%/", "./Data/Classesgrey25%/")
     # IR.resize_all("./Data/Classes/", "./Data/Classes25%/", 25)
     # IR.convert_folder_classes("./Data/Classes25%/", "25%.pkl")
-    data = IR.load_data("./Data/grey10%parallel.pkl")
+    data = IR.load_data("./Data/grey10%.pkl")
     print(data[0])
     v, h = np.shape(data)
     np.random.shuffle(data)
     train, test, validate = np.split(data, [int(.6 * v), int(.9 * v)])
     X_train = (train[:, :h - 1]) / 255
-    X_train = X_train.reshape(np.shape(X_train)[0], 288, 216, 1)
+    X_train = X_train.reshape(np.shape(X_train)[0], 216, 288, 1)
     y_train = train[:, h - 1]
     X_test = (test[:, :h - 1]) / 255
-    X_test = X_test.reshape(np.shape(X_test)[0], 288, 216, 1)
+    X_test = X_test.reshape(np.shape(X_test)[0], 216, 288, 1)
     y_test = test[:, h - 1]
     X_validate = (validate[:, :h - 1]) / 255
-    X_validate = X_validate.reshape(np.shape(X_validate)[0], 288, 216, 1)
+    X_validate = X_validate.reshape(np.shape(X_validate)[0], 216, 288, 1)
     y_validate = validate[:, h - 1]
-    # X = data[:, :h - 1].astype(float)
-    # X = X.reshape(v, 288, 216, 1)
-    # y = np.core.defchararray.replace(data[:, h - 1], '-', '')
-    # y = y.astype(int)
-    print(type(X_train[1][1][1][0]))
-    print(type(y_train[1]))
     BC = BoltClassifier()
+    DA = DataAugmentation()
+    X_aug, y_aug = DA.aug_data(X_train, y_train, np.shape(X_train)[0])
 
-    # model = keras.models.load_model("./Models/prelim_model_parallel")
+    # model = tf.keras.models.load_model("./Models/prelim_model")
 
     model = BC.create_model()
-    model.summary()
 
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
+    model.summary()
 
-    history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
-    model.save("./Models/prelim_model_parallel")
+    history = model.fit(X_aug, y_aug, epochs=10, validation_data=(X_test, y_test))
+    model.save("./Models/prelim_model")
 
     y_pred = model.predict(X_test)
     y_pred = np.argmax(y_pred, axis=1)
