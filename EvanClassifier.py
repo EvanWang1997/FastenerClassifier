738d6ea649c9f3c8602d12418217d6667f642f00
Evan Wang <evan7@students.cs.ubc.ca>
2022-04-18 21:44:40 -0700

    Tested Various Convolutional Layers

---
+++ EvanClassifier.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+from keras import layers, models
+
+
+class EvanClassifier:
+
+    def __init__(self):
+        self.model = models.Sequential()
+
+    def create_model(self):
+        self.model = models.Sequential()
+        self.model.add(layers.Conv2D(32, 3, padding="same", activation="relu", input_shape=(288, 216, 1)))
+        self.model.add(layers.AveragePooling2D())
+
+        self.model.add(layers.Conv2D(32, 3, padding="same", activation="relu"))
+        self.model.add(layers.AveragePooling2D())
+
+        self.model.add(layers.Conv2D(64, 3, padding="same", activation="relu"))
+        self.model.add(layers.AveragePooling2D())
+        self.model.add(layers.Dropout(0.4))
+
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(128, activation="relu"))
+        self.model.add(layers.Dense(11, activation="softmax"))
+        return self.model
--- main.py
+++ main.py
@@ -13,32 +13,37 @@ import keras
 from sklearn.metrics import classification_report
 
 from ImageResize import ImageResizer
+from DataRectifier import DataRectifier
 from BoltClassifier import BoltClassifier
+from EvanClassifier import EvanClassifier
 from RandomForestModels import RandomForestModels
 
 if __name__ == '__main__':
     np.random.seed(69420)
     IR = ImageResizer()
-    data = IR.load_data("./Data/grey10%.pkl")
-    v, h = np.shape(data)
-    np.random.shuffle(data)
-    train, test, validate = np.split(data, [int(.6 * v), int(.9 * v)])
-    X_train = (train[:, :h - 1]) / 255
-    X_train = X_train.reshape(np.shape(X_train)[0], 288, 216, 1)
-    y_train = train[:, h - 1]
-    X_test = (test[:, :h - 1]) / 255
-    X_test = X_test.reshape(np.shape(X_test)[0], 288, 216, 1)
-    y_test = test[:, h - 1]
-    X_validate = (validate[:, :h - 1]) / 255
-    X_validate = X_validate.reshape(np.shape(X_validate)[0], 288, 216, 1)
-    y_validate = validate[:, h - 1]
-    BC = BoltClassifier()
+    DR = DataRectifier()
+    DR.rectifier_data_float("./ValidationSet10%Grey/ValidationSet")
+    # data = IR.load_data("./Data/grey10%parallel.pkl")
+    # v, h = np.shape(data)
+    # np.random.shuffle(data)
+    # train, test, validate = np.split(data, [int(.6 * v), int(.9 * v)])
+    # X_train = (train[:, :h - 1]) / 255
+    # X_train = X_train.reshape(np.shape(X_train)[0], 288, 216, 1)
+    # y_train = train[:, h - 1]
+    # X_test = (test[:, :h - 1]) / 255
+    # X_test = X_test.reshape(np.shape(X_test)[0], 288, 216, 1)
+    # y_test = test[:, h - 1]
+    # X_validate = (validate[:, :h - 1]) / 255
+    # X_validate = X_validate.reshape(np.shape(X_validate)[0], 288, 216, 1)
+    # y_validate = validate[:, h - 1]
+    # BC = BoltClassifier()
+    # EC = EvanClassifier()
+    # FM = RandomForestModels()
 
-    FM = RandomForestModels()
 
-
-    FM.createModels("./Models/8models", 3, BC.create_model, X_train, y_train, 2, (X_test, y_test))
-    y_pred = FM.predictValues(X_validate)
+    # FM.createModels("./Models/AveragePooling5", 5, EC.create_model, X_train, y_train, 20, (X_test, y_test))
+    # FM.loadModels("./Models/AveragePooling")
+    # y_pred = FM.predictValues(X_validate)
 
     # model = BC.create_model()
     # model.summary()
@@ -50,6 +55,6 @@ if __name__ == '__main__':
     # history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
     # model.save("./Models/prelim_model_5")
 
-    print(y_test)
-    # model.evaluate(X_test, y_test)
-    print(classification_report(y_validate, y_pred))
+    # print(y_test)
+    # # model.evaluate(X_test, y_test)
+    # print(classification_report(y_validate, y_pred))
